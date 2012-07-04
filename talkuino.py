e8acf8997cfe3be3868441247d08e638930efb3e
sauloal <sauloal@yahoo.com.br>
2012-07-04 15:38:49 +0200

    now as lib

--- talkuino.py
+++ talkuino.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python
-import serial
-import sys
-import os
-import time
 import arduinoconnect
-
+import sys
 
 
 
 def main():
-    arduinoId    =   1
-    portNum      =  13
-    val          = 330
-    msgId        =  12
+    arduinoId =   1
+    portNum   =  13
+    val       = 330
+    msgId     =  12
+    arduino   = arduinoconnect.arduinoconnect()
+    #sys.exit(0)
 
-    message1 = writeDigitalPort(arduinoid=arduinoId, port=portNum, value=val)
-    message2 = writeAnalogPort( arduinoid=arduinoId, port=portNum, value=val)
-    message3 = ReadDigitalPort( arduinoid=arduinoId, port=portNum, value=val)
-    message4 = ReadAnalogPort(  arduinoid=arduinoId, port=portNum, value=val)
-    print "SENDING MESSAGE 1",message1
-    print "SENDING MESSAGE 2",message2
-    print "SENDING MESSAGE 3",message3
-    print "SENDING MESSAGE 4",message4
-    
+    #if False:
+    if True:
+      message1 = arduino.writeDigitalPort(arduinoid=arduinoId, port=portNum, value=val)
+      print "SENDING MESSAGE 1",message1
 
+      message2 = arduino.writeAnalogPort( arduinoid=arduinoId, port=portNum, value=val)
+      print "SENDING MESSAGE 2",message2
+
+      message3 = arduino.ReadDigitalPort( arduinoid=arduinoId, port=portNum, value=val)
+      print "SENDING MESSAGE 3",message3
+
+      message4 = arduino.ReadAnalogPort(  arduinoid=arduinoId, port=portNum, value=val)
+      print "SENDING MESSAGE 4",message4 
     
     print "out of the loop"
-    #serW = serial.Serial('/dev/'+tty, baud
-    #serW.write('5')
 
 if __name__ == '__main__': main()
